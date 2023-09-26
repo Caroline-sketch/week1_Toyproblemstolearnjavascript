20d1e354a38e043b852dc6b9f8a21b843ad43112
carolinewanjiru <bancroftwanjiru10000@gmail.com>
2023-09-26 12:25:12 +0300

    completed assignment

--- assignment/deductions.js
+++ assignment/deductions.js
@@ -1,20 +1,43 @@
-function grades(x){
-    
- 
-    if (x >= 79 && x<= 100){
-        console.log("A");
+//Obtaining net pay using PEDMAS 
+
+function netSalaryCalculation(){
+    let gross = 35000;
+    let nssf = 0.06 * gross;
+    let nhif = 950;
+    let PAYE= 0.25 * gross;
+    let deductions =  nssf + nhif + PAYE;
+    let net = gross - deductions;
+    console.log(net);
+  }
+  netSalaryCalculation();
+
+// *********************
+// use of conditional statements
+
+  function netSalaryCalculation() {
+
+    let gross = 100000;
+    let PAYE;
+    if (gross > 24000) {
+      PAYE = 0.04 * gross;
     }
-    else if(x >=60 && x< 70){
-        console.log("B");
+    else if (gross > 24001 && gross < 32333) {
+      PAYE = 0.06 * gross;
     }
-    else if (x => 49 && x<= 59){
-        console.log("C");
+    else if (gross > 32333) {
+      PAYE = 0.08 * gross;
     }
-    else if (x >=40 && x< 49){
-        console.log("D");
+    else {
+      console.log("invalid input");
     }
-else {
-    console.log("E");
-}
-}
-grades(-1);
+ // defining data input
+    let nssf = 0.06 * gross;
+    let nhif = 1200;
+    // adding all deductions to be deucted from gross income 
+    let deductions = nssf + nhif + PAYE;
+// subtracting the deductions from gross income to get net pay
+    let netpay = gross - deductions;
+    console.log(netpay);
+  }
+  
+  netSalaryCalculation();
