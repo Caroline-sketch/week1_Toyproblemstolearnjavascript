e2259a8230b840acc4eb5a3ef411d3dee48c556c
carolinewanjiru <bancroftwanjiru10000@gmail.com>
2023-10-05 07:45:18 +0300

    completed assignment

--- assignment/speed.js
+++ assignment/speed.js
@@ -1,7 +1,16 @@
 //write a function with the function name being speed and he parameters The data being used//
-
+//declaring speed function
 function carSpeed(speed){
+    const limit= 67;
+    // prints "OK" when the speed is less than speed limit 70.
+    if (limit>= 0 && limit< 70 ){
+     console.log("OK");
+    }
+
+    else if ((speed-limit)/5) {
+        console.log("liscense suspended");
+        console.log("demerit");
     
 }
-
- 
+}
+ carSpeed(70);
